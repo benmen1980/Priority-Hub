d50e478cd39709cba2b32edd65f399cd002d25a4
benmen1980 <benmen1@gmail.com>
2020-06-28 01:39:09 +0300

    konimbo form

---
+++ includes/admin/konimbo.php
@@ -0,0 +1,24 @@
+<?php
+
+// Konimbo options
+	echo ('<br><br>');
+
+	?>
+
+    <form action="<?php echo esc_url( admin_url('admin-post.php')); ?>" method="post">
+        <input type="hidden" name="action" value="sync_konimbo">
+        <input type="submit" value="Click here to sync Konimbo to Priority">
+    </form>
+<?php
+	// show active users
+	// show the last sync of each user
+	// make a form to post one order
+	if ( isset( $_GET['post_all'] ) ) {
+		$this->process_all_users();
+	}
+if ( isset( $_POST['submit'] ) ) {
+	echo 'Form submitted!!!';
+}
+
+
+
