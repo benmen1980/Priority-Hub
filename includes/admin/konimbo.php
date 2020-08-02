5b5feabb5c643229c56652f65645d12c46428090
benmen1980 <benmen1@gmail.com>
2020-08-02 21:14:18 +0300

    fix issues error handling

--- includes/admin/konimbo.php
+++ includes/admin/konimbo.php
@@ -9,7 +9,7 @@
         <input type="hidden" name="action" value="sync_konimbo">
         <div><input type="checkbox" name="debug" value="debug"><span>Debug</span></div>
         <div><input type="checkbox" name="generalpart" value="generalpart"><span>Post general item</span></div>
-        <div><input type="text" name="order" value=""><span>Debug Order</span></div>
+        <div><input type="text" name="order" value=""><span>Post sungle Order</span></div>
         <br>
         <?php
         //<input type="submit" value="Click here to sync Konimbo to Priority"> 4567567
@@ -21,16 +21,33 @@
     </form>
 <?php
 if ( isset( $_POST['submit'] ) ) {
-	$responses = Konimbo::instance()->process_all_users();
+    if(isset($_POST['generalpart'])){
+		Konimbo::instance()->generalpart = true;
+    }
+	if(true == $_POST['debug'] || '' != $_POST['order']){
+		Konimbo::instance()->debug = true;
+	}else{
+		Konimbo::instance()->debug = false;
+    }
+	if(isset($_POST['order'])){
+		Konimbo::instance()->order = $_POST['order'];
+	}
+	$user_orders = Konimbo::instance()->get_orders_all_users();
+	foreach($user_orders as $user_id => $orders){
+	    $user = get_user_by('ID',$user_id);
+	    $responses[$user_id] = Konimbo::instance()->process_orders($orders,$user);
+    }
 	$messages =  Konimbo::instance()->processResponse($responses);
+	if(empty($messages)){
+	    wp_die('No data to process, you might dont have orders to post or error so check your email.');
+    }
 	foreach($messages as $user_id => $message){
 	    $user = get_user_by('ID',$user_id);
 		if (true == $message['is_error']) {
 			$subject = 'Konimbo Error for user ' . get_user_meta( $user->ID, 'nickname', true );
 		//	Konimbo::instance()->sendEmailError($subject, $message);
-        echo $message['message'];
 		}
-
+    echo $message['message'];
     }
 }
 
