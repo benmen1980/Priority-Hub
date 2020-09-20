ffba7468aa59d3a9c98f721d58cb2adf0ff6defa
benmen1980 <benmen1@gmail.com>
2020-09-21 02:09:17 +0300

    add otc

--- Shopify/shopify-class.php
+++ Shopify/shopify-class.php
@@ -57,7 +57,6 @@ function get_orders_by_user( $user ) {
         update_user_meta( $user->ID, 'shopify_last_sync_date', $new_sync_time );
     }
     $filter_status = '&payment_status=אשראי - מלא';
-    //$konimbo_url   = $konimbo_base_url . $order_id . $token . $orders_limit . $filter_status;
     // debug url
     if ($this->debug) {
         $shopify_base_url = 'https://'.get_user_meta( $user->ID, 'shopify_url', true ).'/admin/api/2020-04/orders.json?name='.$this->order;
@@ -115,8 +114,15 @@ if(empty($orders)){
 return ;
 }
 foreach ( $orders as $order ) {
-//	echo 'Starting process order ' . $order->id . '<br>';
-$response = $this->post_order_to_priority( $order, $user );
+    switch($this->document){
+        case 'order':
+            $response = $this->post_order_to_priority( $order, $user );
+            break;
+        case 'otc':
+            $response = $this->post_otc_to_priority($order,$user);
+            break;
+    }
+
 $responses[$order->id]= $response;
 $response_body = json_decode($response['body']);
 $body_array = json_decode( $response["body"], true );
@@ -266,6 +272,115 @@ $response = $this->makeRequest( 'POST', 'ORDERS', [ 'body' => json_encode( $data
 
 return $response;
 }
+function post_otc_to_priority( $order, $user ) {
+
+        $cust_number = get_user_meta( $user->ID, 'walk_in_customer_number', true );
+        $data        = [
+            'CUSTNAME' => $cust_number,
+            'CDES'     => $order->billing_address->first_name.' '.$order->billing_address->last_name,
+//'CURDATE'  => date('Y-m-d', strtotime($order->get_date_created())),
+            'BOOKNUM'  => 'SHOPIFY'.$order->name,
+//'DETAILS'  => trim(preg_replace('/\s+/', ' ', $order->note))
+        ];
+// billing customer details
+        $customer_data                = [
+            'PHONE' => $order->customer->phone,
+            'EMAIL' => $order->customer->email,
+            'ADRS'  => $order->default_address->address1,
+        ];
+        $data['EINVOICESCONT_SUBFORM'][] = $customer_data;
+// shipping
+        $shipping_data           = [
+            'NAME'      => $order->shipping_address->first_name.' '.$order->shipping_address->last_name,
+            'CUSTDES'   => $order->shipping_address->first_name.' '.$order->shipping_address->last_name,
+            'PHONENUM'  => $order->shipping_address->phone,
+            'ADDRESS'   => $order->shipping_address->address1,
+        ];
+        $data['SHIPTO2_SUBFORM'] = $shipping_data;
+
+// get ordered items
+        foreach ( $order->line_items as $item ) {
+            $partname = $item->sku;
+// debug
+            if (!empty($this->generalpart)) {
+                $partname = '000';
+            }
+            $second_code = isset($item->second_code) ? $item->second_code : '';
+            $unit_price = isset($item->unit_price) ? (float) $item->unit_price : 0.0;
+            $quantity = isset($item->quantity) ? (int)$item->quantity : 0;
+            $data['EINVOICEITEMS_SUBFORM'][] = [
+                'PARTNAME' => $partname,
+                'TQUANT'   => (int) $item->quantity,
+                'TOTPRICE' => (float)$item->price,
+//  if you are working without tax prices you need to modify this line Roy 7.10.18
+//'REMARK1'  =>$second_code,
+//'DUEDATE' => date('Y-m-d', strtotime($campaign_duedate)),
+            ];
+        }
+
+// get discounts as items
+        $discount =  $order->total_discount_set->presentment_money;
+        $discount_partname = '000';
+        if(!empty($discount)){
+            foreach ( $order->discounts as $item ) {
+                $data['EINVOICEITEMS_SUBFORM'][] = [
+                    'PARTNAME' => $discount_partname,
+                    'TQUANT'   => (int)-1,
+                    'TOTPRICE' => (float) $discount->price * - 1.0,
+                    //'PDES'     => $item->title,
+                    //'DUEDATE' => date('Y-m-d', strtotime($campaign_duedate)),
+                ];
+            }
+        }
+// shipping rate
+
+        $shipping = $order->total_shipping_price_set->presentment_money;
+
+        $data['EINVOICEITEMS_SUBFORM'][] = [
+// 'PARTNAME' => $this->option('shipping_' . $shipping_method_id, $order->get_shipping_method()),
+            'PARTNAME' => '000',
+            'PDES'     => '',
+            'TQUANT'   => (int)1,
+            'TOTPRICE' => (float)$shipping->amount
+        ];
+
+
+
+// payment info
+        $payment              = $order->payment_details;
+        $shopify_cards_dictionary   = array(
+            1 => '1',  // Isracard
+            2 => '5',  // Visa
+            3 => '3',  // Diners
+            4 => '4',  // Amex
+            5 => '5',  // JCB
+            6 => '6'   // Leumi Card
+        );
+        $payment_code               = $payment->credit_card_bin;
+        $data['EPAYMENT2_SUBFORM'][] = [
+            'PAYMENTCODE' => $payment_code,
+            'QPRICE'      => (float) $order->total_price_set->presentment_money->amount,
+// Shopify can handle multi paymnets so this might be modified
+//'PAYACCOUNT'  => '',
+//'PAYCODE'     => '',
+            'PAYACCOUNT'  => $payment->credit_card_number,
+//'VALIDMONTH'  => $credit_cart_payments->card_expiration,
+//'CCUID'       => $credit_cart_payments->credit_cart_token,
+//'CONFNUM'     => $credit_cart_payments->order_confirmation_id,
+//'ROYY_NUMBEROFPAY' => $order_payments,
+//'FIRSTPAY' => $order_first_payment,
+//'ROYY_SECONDPAYMENT' => $order_periodical_payment
+
+        ];
+
+// make request
+//PriorityAPI\API::instance()->run();
+// make request
+//echo json_encode($data);
+        $response = $this->makeRequest( 'POST', 'EINVOICES', [ 'body' => json_encode( $data ) ], $user );
+
+        return $response;
+    }
 public function processResponse($responses){
 $response3 = null;
 if(empty($responses)){
@@ -432,7 +547,7 @@ function register_tag() {
 public function sendEmailError($subject = '', $error = '')
 {
 $user = wp_get_current_user();
-$emails  = [ $user->user_email ];
+$emails  = [$user->user_email];
 $bloguser = get_users('role=Administrator')[0];
 array_push($emails,$bloguser->user_email);
 
