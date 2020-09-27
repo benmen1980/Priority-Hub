20ea98ea6996971f2330c02046e0f4d9101768f8
benmen1980 <benmen1@gmail.com>
2020-09-27 03:51:30 +0300

    sync receipts

--- Konimbo/konimbo.php
+++ Konimbo/konimbo.php
@@ -9,7 +9,16 @@
         <input type="hidden" name="action" value="sync_konimbo">
         <div><input type="checkbox" name="debug" value="debug"><span>Debug</span></div>
         <div><input type="checkbox" name="generalpart" value="generalpart"><span>Post general item</span></div>
-        <div><input type="text" name="order" value=""><span>Post sungle Order</span></div>
+        <div>
+            <select value="" name="konimbo_document" id="shopify_document">
+                <option selected="selected"></option>
+                <option value="order">Order</option>
+                <option value="receipt">Receipts</option>
+                <option value="invoice">Sales Invoice</option>
+                <option value="orderreceipt">Order + Receipt</option>
+            </select>
+            <label>Select Priority Entity target</label></div>
+        <input type="text" name="order" value=""><span>Post single Order</span></div>
         <br>
         <?php
         //<input type="submit" value="Click here to sync Konimbo to Priority"> 4567567
@@ -21,6 +30,7 @@
     </form>
 <?php
 if ( isset( $_POST['submit'] ) ) {
+    Konimbo::instance()->document = $_POST['konimbo_document'];
     if(isset($_POST['generalpart'])){
 		Konimbo::instance()->generalpart = true;
     }
@@ -35,7 +45,14 @@ if ( isset( $_POST['submit'] ) ) {
 	$user_orders = Konimbo::instance()->get_orders_all_users();
 	foreach($user_orders as $user_id => $orders){
 	    $user = get_user_by('ID',$user_id);
-	    $responses[$user_id] = Konimbo::instance()->process_orders($orders,$user);
+        switch(Konimbo::instance()->document){
+            case 'order':
+                $responses[$user_id] = Konimbo::instance()->process_orders($orders,$user);
+                break;
+            case 'receipt':
+                $responses[$user_id] = Konimbo::instance()->process_receipts($orders,$user);
+                break;
+        }
     }
 	$messages =  Konimbo::instance()->processResponse($responses);
 	if(empty($messages)){
