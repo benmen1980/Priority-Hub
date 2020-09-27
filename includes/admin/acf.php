b12362e4a8653ca5f956b89a626afd918f74b782
benmen1980 <benmen1@gmail.com>
2020-09-27 03:50:37 +0300

    Konimbo receipt sync data and the funciton to export acf to json

--- includes/admin/acf.php
+++ includes/admin/acf.php
@@ -1,200 +1,200 @@
 <?php
 if( function_exists('acf_add_local_field_group') ):
 
-	acf_add_local_field_group(array(
-		'key' => 'group_5ef3cac26241d',
-		'title' => 'Priority API settings',
-		'fields' => array(
-			array(
-				'key' => 'field_5ef3cac271014',
-				'label' => 'Application',
-				'name' => 'application',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => 'tabula.ini,2',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5ef3cac2713f2',
-				'label' => 'Environment name',
-				'name' => 'environment_name',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5ef3cac271800',
-				'label' => 'URL',
-				'name' => 'url',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5ef3cac271bea',
-				'label' => 'Username',
-				'name' => 'username',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5ef3cac2723a4',
-				'label' => 'Password',
-				'name' => 'password',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5ef3cac27279b',
-				'label' => 'X-App-Id',
-				'name' => 'x-app-id',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5ef3cac272b70',
-				'label' => 'X-App-Key',
-				'name' => 'x-app-key',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-			array(
-				'key' => 'field_5ef3cac272f6a',
-				'label' => 'SSL verify',
-				'name' => 'ssl_verify',
-				'type' => 'true_false',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'message' => '',
-				'default_value' => 0,
-				'ui' => 0,
-				'ui_on_text' => '',
-				'ui_off_text' => '',
-			),
-			array(
-				'key' => 'field_5ef3cc5964b63',
-				'label' => 'Walk In Customer Number',
-				'name' => 'walk_in_customer_number',
-				'type' => 'text',
-				'instructions' => '',
-				'required' => 0,
-				'conditional_logic' => 0,
-				'wrapper' => array(
-					'width' => '',
-					'class' => '',
-					'id' => '',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'prepend' => '',
-				'append' => '',
-				'maxlength' => '',
-			),
-		),
-		'location' => array(
-			array(
-				array(
-					'param' => 'user_form',
-					'operator' => '==',
-					'value' => 'all',
-				),
-			),
-		),
-		'menu_order' => 1,
-		'position' => 'normal',
-		'style' => 'default',
-		'label_placement' => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen' => '',
-		'active' => true,
-		'description' => '',
-	));
+    acf_add_local_field_group(array(
+        'key' => 'group_5ef3cac26241d',
+        'title' => 'Priority API settings',
+        'fields' => array(
+            array(
+                'key' => 'field_5ef3cac271014',
+                'label' => 'Application',
+                'name' => 'application',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => 'tabula.ini,2',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cac2713f2',
+                'label' => 'Environment name',
+                'name' => 'environment_name',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cac271800',
+                'label' => 'URL',
+                'name' => 'url',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cac271bea',
+                'label' => 'Username',
+                'name' => 'username',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cac2723a4',
+                'label' => 'Password',
+                'name' => 'password',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cac27279b',
+                'label' => 'X-App-Id',
+                'name' => 'x-app-id',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cac272b70',
+                'label' => 'X-App-Key',
+                'name' => 'x-app-key',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cac272f6a',
+                'label' => 'SSL verify',
+                'name' => 'ssl_verify',
+                'type' => 'true_false',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'message' => '',
+                'default_value' => 0,
+                'ui' => 0,
+                'ui_on_text' => '',
+                'ui_off_text' => '',
+            ),
+            array(
+                'key' => 'field_5ef3cc5964b63',
+                'label' => 'Walk In Customer Number',
+                'name' => 'walk_in_customer_number',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'user_form',
+                    'operator' => '==',
+                    'value' => 'all',
+                ),
+            ),
+        ),
+        'menu_order' => 1,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
 
     acf_add_local_field_group(array(
         'key' => 'group_5f4ffe55a88fe',
@@ -375,7 +375,15 @@ if( function_exists('acf_add_local_field_group') ):
                 'type' => 'text',
                 'instructions' => 'Enter the token you received from customer service.',
                 'required' => 0,
-                'conditional_logic' => 0,
+                'conditional_logic' => array(
+                    array(
+                        array(
+                            'field' => 'field_5eebdfb0162a0',
+                            'operator' => '==',
+                            'value' => '1',
+                        ),
+                    ),
+                ),
                 'wrapper' => array(
                     'width' => '',
                     'class' => '',
@@ -389,22 +397,53 @@ if( function_exists('acf_add_local_field_group') ):
             ),
             array(
                 'key' => 'field_5eebdeb08a754',
-                'label' => 'Last Sync time',
-                'name' => 'konimbo_last_sync_time',
-                'type' => 'text',
+                'label' => 'Orders Last Sync time',
+                'name' => 'konimbo_orders_last_sync_time',
+                'type' => 'date_time_picker',
                 'instructions' => '',
                 'required' => 0,
-                'conditional_logic' => 0,
+                'conditional_logic' => array(
+                    array(
+                        array(
+                            'field' => 'field_5eebdfb0162a0',
+                            'operator' => '==',
+                            'value' => '1',
+                        ),
+                    ),
+                ),
                 'wrapper' => array(
                     'width' => '',
                     'class' => '',
                     'id' => '',
                 ),
-                'default_value' => '2020-06-15T00:00:00Z',
-                'placeholder' => '',
-                'prepend' => '',
-                'append' => '',
-                'maxlength' => '',
+                'display_format' => 'd/m/Y g:i a',
+                'return_format' => 'd/m/Y g:i a',
+                'first_day' => 1,
+            ),
+            array(
+                'key' => 'field_5f6fd6850a6d9',
+                'label' => 'Receipts Last Sync Time',
+                'name' => 'konimbo_receipts_last_sync_time',
+                'type' => 'date_time_picker',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => array(
+                    array(
+                        array(
+                            'field' => 'field_5eebdfb0162a0',
+                            'operator' => '==',
+                            'value' => '1',
+                        ),
+                    ),
+                ),
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'display_format' => 'd/m/Y g:i a',
+                'return_format' => 'd/m/Y g:i a',
+                'first_day' => 0,
             ),
         ),
         'location' => array(
@@ -427,3 +466,35 @@ if( function_exists('acf_add_local_field_group') ):
     ));
 
 endif;
+
+// export to JSON, use this code to export the php code to JSON file that can be use to load
+/*
+$groups = acf_get_local_field_groups();
+$json = [];
+
+foreach ($groups as $group) {
+    // Fetch the fields for the given group key
+    $fields = acf_get_local_fields($group['key']);
+
+    // Remove unecessary key value pair with key "ID"
+    unset($group['ID']);
+
+    // Add the fields as an array to the group
+    $group['fields'] = $fields;
+
+    // Add this group to the main array
+    $json[] = $group;
+}
+
+$json = json_encode($json, JSON_PRETTY_PRINT);
+// Optional - echo the JSON data to the page
+echo "<pre>";
+echo $json;
+echo "</pre>";
+
+// Write output to file for easy import into ACF.
+// The file must be writable by the server process. In this case, the file is located in
+// the current theme directory.
+$file = get_template_directory() . '/acf-import.json';
+file_put_contents($file, $json );
+*/
