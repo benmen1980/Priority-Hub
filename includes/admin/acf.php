5eff8b5ac6ad4a5533dbe6d388f5df67ddefbea7
benmen1980 <benmen1@gmail.com>
2020-10-02 00:59:37 +0300

    add the Konimbo order post type custom fields.

--- includes/admin/acf.php
+++ includes/admin/acf.php
@@ -559,7 +559,68 @@ if( function_exists('acf_add_local_field_group') ):
         'active' => true,
         'description' => '',
     ));
-
+    // Konimbo order post type fields
+    acf_add_local_field_group(array(
+        'key' => 'group_5f764a3e41095',
+        'title' => 'Konimbo Orders',
+        'fields' => array(
+            array(
+                'key' => 'field_5f764bec88181',
+                'label' => 'Konimbo Order Number',
+                'name' => 'konimbo_order_number',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'default_value' => '',
+                'placeholder' => '',
+                'prepend' => '',
+                'append' => '',
+                'maxlength' => '',
+            ),
+            array(
+                'key' => 'field_5f764a48bf96e',
+                'label' => 'Receipts posted ?',
+                'name' => 'konimbo_is_receipts_posted',
+                'type' => 'true_false',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'message' => '',
+                'default_value' => 0,
+                'ui' => 0,
+                'ui_on_text' => '',
+                'ui_off_text' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'konimbo_order',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
 
 endif;
 
