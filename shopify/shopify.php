9681535d70e248f9b367c4584ce246ea1abfb75d
benmen1980 <benmen1@gmail.com>
2020-10-18 03:30:05 +0300

    central class

--- shopify/shopify.php
+++ shopify/shopify.php
@@ -33,13 +33,13 @@ if ( isset( $_POST['submit'] ) && isset($_POST['shopify_username'])&& isset($_PO
     $user = get_user_by('login',$_POST['shopify_username']);
     $activate_sync = get_user_meta( $user->ID, 'shopify_activate_sync',true );
     if(!$user){
-        echo 'Username does not exists';
+        echo 'Username does not exists<br>';
     }
     if(!$activate_sync){
         echo 'User does not set to integrate with Shopify';
     }
-    $shopify = new Shopify;
-    $message = $shopify->post_user_by_id($user->ID,$_POST['shopify_order'],$_POST['shopify_document']);
+    $shopify = new Shopify($_POST['shopify_document'],$_POST['shopify_username']);
+    $message = $shopify->post_user_by_username($_POST['shopify_username'],$_POST['shopify_order'],$_POST['shopify_document']);
     echo $message['message'];
 }
 ?>
