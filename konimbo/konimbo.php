0cace736fa05012461cd6b9e6a03148ca9c03821
benmen1980 <benmen1@gmail.com>
2020-10-18 03:29:41 +0300

    central class

--- konimbo/konimbo.php
+++ konimbo/konimbo.php
@@ -39,7 +39,7 @@ if ( isset( $_POST['submit'] ) && isset($_POST['konimbo_username'])&& isset($_PO
     if(!$activate_sync){
         echo 'User does not set to integrate with Konimbo';
     }
-    $konimbo = new Konimbo();
+    $konimbo = new Konimbo($_POST['konimbo_document'],$_POST['konimbo_username']);
     $message = $konimbo->post_user_by_username($_POST['konimbo_username'],$_POST['konimbo_order'],$_POST['konimbo_document']);
     echo $message['message'];
 }
